c5ad2e5b121ad9238a5d5d0ca4b47a8de0f4d259
Saikat Bhattacharya <99088080+bhattacharyasaikat@users.noreply.github.com>
2024-11-09 14:18:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Tiu"
+    return "TIU is the best university in WB!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
